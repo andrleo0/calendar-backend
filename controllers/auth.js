b592abb39d7c7fcbc517788e4bc693d81fe9686f
Andres Guerrero <aguerrero@fi.uba.ar>
2024-08-28 16:38:37 -0300

    start calendar-backend

---
+++ controllers/auth.js
@@ -0,0 +1,45 @@
+
+const { response } = require('express');
+
+
+const crearUsuario = (req, res = response) => {
+
+    const { email, name, password } = req.body;
+    
+    res.json({
+        ok: true,
+        msg: 'registro',
+        name,
+        email,
+        password
+    });
+
+}
+
+const loginUsuario = (req, res = response) => {
+
+    const { email , password } = req.body;
+
+    res.json({
+        ok: true,
+        msg: 'login',
+        email,
+        password
+    });
+
+}
+
+const revalidarToken = (req, res = response) => {
+
+    res.json({
+        ok: true,
+        msg: 'renew'
+    });
+
+}
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
